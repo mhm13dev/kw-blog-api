5f3c2748fc249367f6701ce8f34b082f1311b444
Mubashir Hassan <mhm13devk@gmail.com>
2024-05-31 18:13:04 +0500

    docs: added tsdocs for JWT types

--- src/auth/types/jwt.types.ts
+++ src/auth/types/jwt.types.ts
@@ -1,6 +1,9 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { UserRole } from 'src/user/entities';
 
+/**
+ * This object is returned when the `User` login or refresh the tokens
+ */
 @ObjectType()
 export class TokensPair {
   @Field()
@@ -14,6 +17,11 @@ export class TokensPair {
   }
 }
 
+/**
+ * Logged in `User` payload
+ *
+ * This payload is stored in `access_token` and `refresh_token`
+ */
 export class TokenPayload {
   sub: string;
   role: UserRole;
