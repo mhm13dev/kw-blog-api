94f6540a33e5472e6cf222bacb01932386222610
Mubashir Hassan <mhm13devk@gmail.com>
2024-05-31 18:13:04 +0500

    fix: only run create-indexes migration in production

--- migrations/20240513090956-create-indexes.js
+++ migrations/20240513090956-create-indexes.js
@@ -1,12 +1,20 @@
 /**
- * Create indexes for collections
+ * Create indexes for collections for production
  */
 module.exports = {
   async up(db, client) {
+    // Only create indexes in production
+    if (process.env.NODE_ENV !== 'production') {
+      return;
+    }
     await db.collection('users').createIndex({ email: 1 }, { unique: true });
   },
 
   async down(db, client) {
+    // Only drop indexes in production
+    if (process.env.NODE_ENV !== 'production') {
+      return;
+    }
     await db.collection('users').dropIndex('email_1');
   },
 };
