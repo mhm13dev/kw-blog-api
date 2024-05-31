305b83f1db04ae3b9dc0a305bacd4d7a3f889a4f
Mubashir Hassan <mhm13devk@gmail.com>
2024-05-31 18:13:04 +0500

    docs: added tsdocs for GqlAccessTokenGuard and GqlRefreshTokenGuard

--- src/auth/guards/access-token.guard.ts
+++ src/auth/guards/access-token.guard.ts
@@ -3,6 +3,14 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 import { AccessTokenStrategyName } from '../strategies';
 
+/**
+ * Guard to protect GraphQL resolvers with `access_token` strategy
+ *
+ * This guard is responsible for checking the `access_token` from the request headers.
+ * If the token is valid, it will allow the request to proceed.
+ *
+ * @throws `UnauthorizedException` If the `access_token` is invalid
+ */
 @Injectable()
 export class GqlAccessTokenGuard extends AuthGuard(AccessTokenStrategyName) {
   getRequest(context: ExecutionContext) {
